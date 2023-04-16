7fdb36612bc5de41c79c3e1f61782743afe17a6f
nhannguyenvh79 <nhannguyenvh79@gmail.com>
2023-04-16 22:56:03 +0700

    updated

--- nhan/js/scrollAnimation.js
+++ nhan/js/scrollAnimation.js
@@ -1,6 +1,5 @@
 const $scrolls = document.querySelectorAll('.scroll')
-
-window.addEventListener('scroll', () => {
+const animation = () => {
     $scrolls.forEach(function (scroll) {
         if (scroll.getBoundingClientRect().top <= 0.9*document.documentElement.clientHeight) {
             scroll.classList.add('active')
@@ -8,4 +7,7 @@ window.addEventListener('scroll', () => {
             scroll.classList.remove('active')
         }
     })       
-})
+}
+
+window.addEventListener('scroll', animation)
+window.addEventListener('load', animation)
