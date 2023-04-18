9b38c3b952a0311adc4943487bac85f2404557ac
nhannguyenvh79 <nhannguyenvh79@gmail.com>
2023-04-18 23:02:11 +0700

    update

--- nhanNew/p5_6/js/form.js
+++ nhanNew/p5_6/js/form.js
@@ -5,48 +5,97 @@ const $email = document.getElementById('email')
 const $phonenumber = document.getElementById('phonenumber')
 const $subject = document.getElementById('subject')
 const $message = document.getElementById('message')
+const arr = [$firstname, $lastname, $subject, $message, $email, $phonenumber]
 
+////////////////////////////////
 function isEmail(email) {
     const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     return regex.test(email);
 }
+
 function isPhoneNumber(phoneNumber) {
     const regex = /^\d{10}$/; 
     return regex.test(phoneNumber);
 }
 
-
-function alertTrue () {
-    document.getElementById ('alert-1').style.display = 'none';
-    document.getElementById ('alert-2').style.display = 'block';
-}
-function aleartFalse () {
-    document.getElementById ('alert-1').style.display = 'block';
-    document.getElementById ('alert-2').style.display = 'none';
-}
-
-function checkValid (email, phoneNumber) {
-    if (isEmail(email) && isPhoneNumber(phoneNumber)) {
-        alertTrue()
-        return true;
-    } else if (!isPhoneNumber(phoneNumber)) {
-        document.getElementById('error').innerHTML = 'Phone Number'
-        aleartFalse()
-        return false;
-    } else if (!isEmail(email)) {
-        document.getElementById('error').innerHTML = 'Email Eddress'
-        aleartFalse()
-        return false;
+function checkInput () {
+    arr.forEach((el) => {
+        if (el.value == '') {
+            el.style.border = '1px solid red'
+            document.getElementById('alert-1').innerHTML = 'Please complete all information!'
+            document.getElementById ('alert-1').style.opacity = '1';
+        } else if (!(el.value == '')) {
+            el.style.border = '1px solid green'
+        } 
+    })
+    if (arr.every((el) => el.value != '')) {
+        document.getElementById ('alert-1').style.opacity = '0';
     }
-}  
+}
+
+function checkPhoneNumber () {
+    $phonenumber.addEventListener("input",() => {
+        if (isPhoneNumber($phonenumber.value)) {
+            $phonenumber.style.border = '1px solid green'
+            document.getElementById('alert-1').innerHTML = 'Please complete all information!'
+        } else {
+            $phonenumber.style.border = '1px solid red'
+            document.getElementById('alert-1').innerHTML = 'Please enter the correct <span> Phone Number </span>'
+            document.getElementById ('alert-1').style.opacity = '1';
+        }
+      });
+}
+  
+function checkMail () {
+    $email.addEventListener("input",() => {
+        if (isEmail($email.value)) {
+            $email.style.border = '1px solid green'
+            document.getElementById('alert-1').innerHTML = 'Please complete all information!'
+        } else {
+            $email.style.border = '1px solid red'
+            document.getElementById('alert-1').innerHTML = 'Please enter the correct <span> Email </span>'
+            document.getElementById ('alert-1').style.opacity = '1';
+        }
+      });
+}
+
+function backHome () {
+    let i = 5;
+    setInterval(() => {
+        document.getElementById('alert-2').innerHTML = `Your information has been sent, Page will reload after ${i} seconds`
+        document.getElementById('alert-2').style.opacity = '1'
+        i--
+        if (i == 0) {
+            clearInterval()
+            location.reload();
+        }
+    }, 1000)
+}
+
+//////////////////////////////////
+arr.forEach((el) => {
+    el.oninput = () => {checkInput()}
+})
 
 
 $btn.addEventListener('click', (event) => {
     event.preventDefault();
 })
+
 $btn.onclick = () => {
-    checkValid($email.value, $phonenumber.value)
-    if (checkValid($email.value, $phonenumber.value)) {
+    if (!isEmail($email.value)) {
+        $email.style.border = '1px solid red'
+        document.getElementById('alert-1').innerHTML = 'Please enter the correct <span> Email </span>'
+        document.getElementById ('alert-1').style.opacity = '1';
+        checkMail ()
+    } else if (!isPhoneNumber($phonenumber.value)) {
+        $phonenumber.style.border = '1px solid red'
+        document.getElementById('alert-1').innerHTML = 'Please enter the correct <span> Phone Number </span>'
+        document.getElementById ('alert-1').style.opacity = '1';
+        checkPhoneNumber ()
+    }
+    if (arr.every((el) => el.value != '') && isPhoneNumber($phonenumber.value) && isEmail($email.value)) {
+        $btn.disabled = true;
         let object = {
             firstname: $firstname.value,
             lastname: $lastname.value,
@@ -55,29 +104,10 @@ $btn.onclick = () => {
             subject: $subject.value,
             message: $message.value,
         }
-        console.log(object)
         let objectToJson = JSON.stringify(object)
         let i = localStorage.length;
         i++
         localStorage.setItem(`user ${i}`, objectToJson)
         backHome()
     }
-    
-}
-// const value = localStorage.getItem("Nhân")
-// const jsonToObject = JSON.parse(value)
-// console.log(jsonToObject)
-
-const $backHome = document.getElementById("back-home")
-function backHome () {
-    let i = 5;
-    setInterval(() => {
-        $backHome.innerHTML = `Page will reload after ${i} seconds`
-        $backHome.style.display = 'block'
-        i--
-        if (i == 0) {
-            clearInterval()
-            location.reload();
-        }
-    }, 1000)
 }
